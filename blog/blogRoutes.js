c8e6eda6822e17da6d8bda1c5621e063051587a5
ndurbin <nick.durbin@gmail.com>
2019-11-10 09:27:06 -0500

    Setup the basic index file and imported the blogRoutes.

---
+++ blog/blogRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const database = require('../data/db');
+const router = express.Router()
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const cors = require('cors')
+const blogRouter = require('./blog/blogRoutes')
+const server = express()
+
+server.use(express.json())
+server.use('/api/posts', blogRouter)
+server.use(cors())
+
+server.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
