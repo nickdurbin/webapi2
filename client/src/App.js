ff99394cc63c05d904c7397092c3db04f8d15456
nick.durbin <nick.durbin@gmail.com>
2019-12-06 19:11:26 -0500

    Built out a very simple view for the posts in the frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/posts')
+      .then(res => {
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err.response, 'No posts for you!')
+      })
+  })
   return (
     <div className="App">
-    
+      {posts.map(post => {
+        return (
+          <div key={post.id} className='postContainer'>
+            <h2>Posts</h2>
+            <h3>{post.title}</h3>
+            <h3>{post.contents}</h3>
+          </div>
+        )
+      })}
     </div>
   );
 }
