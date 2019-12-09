7fea23e707d0d2fdcdf85d9356e641560ff7df14
nick.durbin <nick.durbin@gmail.com>
2019-12-09 18:44:53 -0500

    Added variables for port and host.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ server.get("/", (req, res) => {
   res.json({ message: "Server is running!" });
 });
 
+const port = 5000
+const host = 'http://localhost.com/'
+
 server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log(`Server is running on ${host} ${port}`);
 });
